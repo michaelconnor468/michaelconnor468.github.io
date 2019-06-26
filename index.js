300150bb89006e64529fcb987d1dbfa31dfbfda2
michaelconnor468 <michaelconnor468@gmail.com>
2019-06-26 15:41:39 -0400

    made navigation color change on hover in js

---
+++ index.js
@@ -0,0 +1,3 @@
+
+document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.color = '#cccccc';} , false);
+document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.color = '#ffffff';} , false);
