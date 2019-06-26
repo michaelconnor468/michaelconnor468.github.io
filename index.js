b12cafc07088bc99f1bd70fabf1688d7f4ce5f9b
michaelconnor468 <michaelconnor468@gmail.com>
2019-06-26 15:51:41 -0400

    added debugging alert for javascript errors

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-
+window.addEventListener('error', function() {this.window.alert("Error loading dynamic functionality")}, false);
 document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.color = '#cccccc';} , false);
 document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.color = '#ffffff';} , false);
