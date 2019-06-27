22288dd56e5a3b62a1b06cad8758c27b86f0e799
michaelconnor468 <michaelconnor468@gmail.com>
2019-06-26 20:33:28 -0400

    inserted updated resume

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var DEBUG = true;
 
 if(DEBUG){window.addEventListener('error', function() {this.window.alert("Error loading dynamic functionality")}, false);}
+
 document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.transition = '0s'; e.target.style.color = '#cccccc';} , false);
 document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.transition = '0.5s'; e.target.style.color = '#ffffff';} , false);
