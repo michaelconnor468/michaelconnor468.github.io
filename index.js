5c794ecc8af242f1727fc975ae39ac557102b413
michaelconnor468 <michaelconnor468@gmail.com>
2019-06-27 10:37:39 -0400

    added checking of DOM completion before any js execution

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var DEBUG = true;
 
 if(DEBUG){window.addEventListener('error', function() {this.window.alert("Error loading dynamic functionality")}, false);}
 
-document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.transition = '0s'; e.target.style.color = '#cccccc';} , false);
-document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.transition = '0.5s'; e.target.style.color = '#ffffff';} , false);
+$(document).ready( function() {
+    document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.transition = '0s'; e.target.style.color = '#cccccc';} , false);
+    document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.transition = '0.5s'; e.target.style.color = '#ffffff';} , false);
+});
