18c7feba06f3098fda81a25ff00706488338e2ce
michaelconnor468 <michaelconnor468@gmail.com>
2020-06-11 15:53:17 -0400

    chnaged link color transition time to 0.6s

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ if(DEBUG){window.addEventListener('error', function() {this.window.alert("Error 
 
 $(document).ready( function() {
     document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.transition = '0s'; e.target.style.color = '#cccccc';} , false);
-    document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.transition = '0.5s'; e.target.style.color = '#ffffff';} , false);
+    document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.transition = '0.6s'; e.target.style.color = '#ffffff';} , false);
 });
