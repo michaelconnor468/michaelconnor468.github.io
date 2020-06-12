aeb26f9dacde02c65944a84b45563f14ebe1156b
michaelconnor468 <michaelconnor468@gmail.com>
2020-06-12 13:32:10 -0400

    added gradient transition when hovering over links

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var DEBUG = true;
 if(DEBUG){window.addEventListener('error', function() {this.window.alert("Error loading dynamic functionality")}, false);}
 
 $(document).ready( function() {
-    document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.transition = '0s'; e.target.style.color = '#cccccc';} , false);
+    document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.transition = '0.4s'; e.target.style.color = '#cccccc';} , false);
     document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.transition = '0.6s'; e.target.style.color = '#ffffff';} , false);
 });
