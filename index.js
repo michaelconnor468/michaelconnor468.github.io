68256088c009a3cb7748e64cb3587dd439247244
michaelconnor468 <michaelconnor468@gmail.com>
2020-06-23 20:56:05 -0400

    removed jquery

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var DEBUG = true;
 
 if(DEBUG){window.addEventListener('error', function() {this.window.alert("Error loading dynamic functionality")}, false);}
 
-$(document).ready( function() {
+function setup() {
     document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.transition = '0.4s'; e.target.style.color = '#cccccc';} , false);
     document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.transition = '0.6s'; e.target.style.color = '#ffffff';} , false);
-});
+}
+
+window.addEventListener('load', setup, false);
