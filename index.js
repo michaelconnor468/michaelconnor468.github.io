4322d4f309fc4e6bebe6e1d4ef5fb3439fdaa063
michaelconnor468 <michaelconnor468@gmail.com>
2020-06-24 20:48:14 -0400

    made header bold

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var DEBUG = true;
 
-if(DEBUG){window.addEventListener('error', function() {this.window.alert("Error loading dynamic functionality")}, false);}
+if(DEBUG){window.addEventListener('error', function() {this.window.alert("Javascript Debug Exception")}, false);}
+
+function swapPageBody(event, page) {
+    return false;
+}
 
 function setup() {
-    document.getElementById('header_links').addEventListener('mouseover', function(e) {e.target.style.transition = '0.4s'; e.target.style.color = '#cccccc';} , false);
-    document.getElementById('header_links').addEventListener('mouseout', function(e) {e.target.style.transition = '0.6s'; e.target.style.color = '#ffffff';} , false);
+    // Navigation events
+    document.getElementById('header_links').addEventListener('mouseover', function(event) {event.target.style.transition = '0.4s'; event.target.style.color = '#cccccc';} , false);
+    document.getElementById('header_links').addEventListener('mouseout', function(event) {event.target.style.transition = '0.6s'; event.target.style.color = '#ffffff';} , false);
 }
 
 window.addEventListener('load', setup, false);
